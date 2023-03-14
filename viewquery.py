bde42356d70d1e8b9c0b3bf419463b181acfd991
Truongphi20 <96680644+Truongphi20@users.noreply.github.com>
2023-03-14 10:56:51 +0700

    Find some bugs

--- viewquery.py
+++ viewquery.py
@@ -4,7 +4,9 @@ import concurrent.futures as futures
 import pandas as pd
 import webbrowser
 import argparse
-
+import numpy as np
+from lxml import etree
+from tqdm import tqdm
 
 # Initialize parser
 parser = argparse.ArgumentParser()
@@ -23,15 +25,26 @@ def RepuScore_html(pub_id): # Get reputation score from html
     
 	payload = f"https://pubmed.ncbi.nlm.nih.gov/?linkname=pubmed_pubmed_citedin&from_uid={pub_id}"
 	# print(pub_id,"\n")
-	page = requests.get(payload, timeout=10)
+	page = requests.get(payload, timeout=(4,3))
 	html_doc = page.text
+	# print(re.findall(r"(<span class=\"value\">\d+</span>)", html_doc), pub_id)
 	total = re.findall(r"<span class=\"value\">(\d+)</span>", html_doc)
+	# print(html_doc)
 	if len(total) > 0:
 		# print(pub_id, total[0])
-		return int(total[0])
+		return int(total[0])-1
 	else:
-		return 0
-# print(RepuScore_html("17459724"))
+		tree = etree.HTML(html_doc)
+		message = tree.xpath('//*[@id="article-top-actions-bar"]/div/div/div[1]/span/text()')[0]
+		# print(message)
+		num_one = re.search(r"Found (\d+) result for", message).group(1)
+		# print(num_one)
+		if num_one == '1':
+			return 0
+		else:
+			raise Exception("Can't find")
+
+# print(RepuScore_html("36836348"))
 
 def Breakline(string, num_char):
 
@@ -94,7 +107,7 @@ class pub_search():
 								'retmax':args.get, 'sort':"pub_date", 'usehistory':"y"}
 		while True:
 			try:
-				handle = requests.get(esum_url,params=payload, timeout=20)
+				handle = requests.get(esum_url,params=payload, timeout=10)
 				#print(handle.url)
 			except:
 				continue
@@ -131,30 +144,38 @@ class pub_search():
 
 	def reputation(self):
 
-		future_list = []
-		with futures.ThreadPoolExecutor() as executor:
-			for ids in self.pub_ids:
-				future_list.append(executor.submit(RepuScore_html, ids))
-		
-		repu = []
-		for future in future_list:
-			while True:
-				try:
-					rs = future.result()
-					# print(rs)
-					repu.append(rs)
-				except:
-					continue
-				break
+		bucket = self.pub_ids.copy()
+		# print(bucket)
+		reputation = []
+		pbar = tqdm(total = min([int(args.get), int(self.counts)]))
 
-		return repu 
+		while len(bucket) > 0:
+			
+			with futures.ThreadPoolExecutor() as executor:
+				future_to_ids = {executor.submit(RepuScore_html, ids): ids for ids in bucket}
+
+				for future in futures.as_completed(future_to_ids):
+					ids = future_to_ids[future]
+					try:
+						data = future.result()
+						reputation.append((ids, data))
+						bucket.remove(ids)
+						pbar.update(1)
+					except:
+						continue
+				# print(bucket)
+		pbar.close()
+		return reputation
 
 # query = "(microorganism[Title/Abstract]) AND (Genetic Engineering[Title/Abstract])"
 query = args.query
+print(f"QUERY: {query}")
 
 handle = pub_search(query)
 
 counts = handle.counts
+print(f'Total of papers: {counts}\n')
+
 pub_id = handle.pub_ids
 # print(pub_id)
 
@@ -164,11 +185,15 @@ titles = handle.title
 reputation = handle.reputation()
 # print(reputation)
 
-df = pd.DataFrame(zip(pub_id, titles, reputation), columns=["ID Pubmed", "Title", "#Cited"])
+dy = pd.DataFrame(reputation, columns=["ID Pubmed", "#Cited"])
+dx = pd.DataFrame(zip(pub_id, titles), columns=["ID Pubmed", "Title"])
+
+df = pd.merge(dx, dy, how="outer", on="ID Pubmed")
+
 df = df.sort_values(by='#Cited', ascending=False)
 df = df.set_index('ID Pubmed')
 # df["Title"] = df["Title"].apply(lambda x: Breakline(x, 70))
-print(df)
+# print(df)
 
 # with open('data.txt', 'w', encoding="utf-8") as writer:
 # 	writer.write(f"QUERY: {query}\n")
